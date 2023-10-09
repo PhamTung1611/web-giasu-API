5d16506c407e9c0a20d454dfd82b95ca6973d92e
tungphph27346 <phamtungate@gmail.com>
2023-10-09 22:48:28 +0700

    create api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeacherClassController;
+use App\Http\Controllers\TeacherSubjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('TeacherSubject')->group(function () {
+    // lấy ra danh sách
+    Route::get('/', [TeacherSubjectController::class, 'index']);
+    //thêm 
+    Route::post('/', [TeacherSubjectController::class, 'store']);
+    //chi tiết
+    Route::get('/{id}', [TeacherSubjectController::class, 'show']);
+    //chỉnh sửa
+    Route::put('/{id}', [TeacherSubjectController::class, 'update']);
+    //xóa
+    Route::delete('/{id}', [TeacherSubjectController::class, 'destroy']);
+});
+
+Route::prefix('TeacherClass')->group(function () {
+    // lấy ra danh sách 
+    Route::get('/', [TeacherClassController::class, 'index']);
+    //thêm 
+    Route::post('/', [TeacherClassController::class, 'store']);
+    //chi tiết
+    Route::get('/{id}', [TeacherClassController::class, 'show']);
+    //chỉnh sửa
+    Route::put('/{id}', [TeacherClassController::class, 'update']);
+    //xóa
+    Route::delete('/{id}', [TeacherClassController::class, 'destroy']);
 });
