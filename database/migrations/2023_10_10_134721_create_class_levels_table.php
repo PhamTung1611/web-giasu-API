e0df16ffbd5b873bc75c7867a19424293b28332e
tungphph27346 <phamtungate@gmail.com>
2023-10-18 10:16:40 +0700

    fix

--- database/migrations/2023_10_10_134721_create_class_levels_table.php
+++ database/migrations/2023_10_10_134721_create_class_levels_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('class_levels', function (Blueprint $table) {
             $table->id();
             $table->string('class');
-            $table->string('subject');
             $table->timestamps();
             $table->softDeletes();
         });
