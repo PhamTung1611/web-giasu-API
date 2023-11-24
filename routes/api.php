b39acf7d2d3bcc4f060dbe69913bb7ada22c80e0
tungphph27346 <phamtungate@gmail.com>
2023-11-24 14:53:32 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -242,5 +242,3 @@ Route::prefix('history')->group(function () {
 
 });
 Route::post('add-info',[AuthController::class,'addInfo']);
-
-Route::post('testTransfer',[HistoryController::class,'transferMoney']);
