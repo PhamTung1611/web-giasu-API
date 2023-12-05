d49f2036c74ec34135f5c75ddd6f71eb35c9e8bf
huytq171020 <147819575+huytq171020@users.noreply.github.com>
2023-12-05 14:24:54 +0700

    Update layout.blade.php

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -250,7 +250,7 @@
                     <div class="multi-level collapse  show " role="list" id="submenu-app" aria-expanded="false">
                         <ul class="flex-column nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="">
+                                <a class="nav-link" href="{{route('search_connect')}}">
                                     <span class="sidebar-text">Kết nối</span>
                                 </a>
                             </li>
