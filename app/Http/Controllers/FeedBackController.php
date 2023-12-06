4f2d6320580a3dd8418f80b72d18000833949a44
tungphph27346 <phamtungate@gmail.com>
2023-12-06 16:09:42 +0700

    fix

--- app/Http/Controllers/FeedBackController.php
+++ app/Http/Controllers/FeedBackController.php
@@ -19,10 +19,13 @@ class FeedBackController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request,HistoryController $historyController)
     {
         //
         $data = FeedBack::create($request->all());
+        if($data->point == 5){
+            $historyController->addCoin($data->id_teacher);
+        }
         if ($data) {
             return response()->json(['message' => 'Success'], 200);
         } else {
