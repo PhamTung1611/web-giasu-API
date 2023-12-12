99e605582fd6f36bc292df418949a9d108efc56e
tungphph27346 <phamtungate@gmail.com>
2023-12-12 23:33:36 +0700

    fix

--- resources/views/mail/rent.blade.php
+++ resources/views/mail/rent.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website GS7</title>
 </head>
+
 <body>
     <h1>{{ $mailData['title'] }}</h1>
-    <p>{{ $mailData['body'] }}</p>
-    <p>Thank you</p>
+    <p>Cảm ơn bạn đã tin tưởng và sử dụng dịch vụ thuê gia sư tại Website Tìm kiếm gia sư GS7, chúng tôi xin thông báo <b style="color: red;">{{ $mailData['body'] }}</b> để nắm rõ hơn về thông tin , bạn vui lòng bấm vào đường link bên dưới để xem chi tiết và kiểm tra toàn bộ thông tin . Chúng tôi cam kết sẽ bảo mật toàn bộ thông tin cá nhân của bạn.
+    Đội ngũ GS7 xin chân thành cảm ơn!</p>
+    <a href="http://localhost:3000/">Đường link website</a>
+    <p>Thank you!</p>
 </body>
+
 </html>
