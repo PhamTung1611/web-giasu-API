7b4e47fe5cf72d374d287dd05f65774b48bbb2a9
tungphph27346 <phamtungate@gmail.com>
2023-12-18 22:42:11 +0700

    fix

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -70,7 +70,7 @@ class ConnectController extends Controller
             ->get();
 
         if ($connect->isEmpty()) {
-            return response()->json(['message' => 'Not found'], 200);
+            return response()->json(['message' => 'Not found'], 404);
         }
 
         foreach ($connect as $item) {
